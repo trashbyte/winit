c5115062091b8e2fdded2171d4944b018cc05339
trashbyte <github@trashbyte.io>
2022-09-10 16:27:44 -0700

    annoying

--- src/platform_impl/windows/event_loop.rs
+++ src/platform_impl/windows/event_loop.rs
@@ -122,15 +122,15 @@ pub struct EventLoopWindowTarget<T: 'static> {
 macro_rules! main_thread_check {
     ($fn_name:literal) => {{
         let thread_id = unsafe { processthreadsapi::GetCurrentThreadId() };
-        if thread_id != main_thread_id() {
-            panic!(concat!(
-                "Initializing the event loop outside of the main thread is a significant \
-                 cross-platform compatibility hazard. If you really, absolutely need to create an \
-                 EventLoop on a different thread, please use the `EventLoopExtWindows::",
-                $fn_name,
-                "` function."
-            ));
-        }
+        // if thread_id != main_thread_id() {
+        //     panic!(concat!(
+        //         "Initializing the event loop outside of the main thread is a significant \
+        //          cross-platform compatibility hazard. If you really, absolutely need to create an \
+        //          EventLoop on a different thread, please use the `EventLoopExtWindows::",
+        //         $fn_name,
+        //         "` function."
+        //     ));
+        // }
     }};
 }
 
